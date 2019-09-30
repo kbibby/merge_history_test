3cd48d65abf0cdf52f5c949b1b148dcb2540330c
kbibby <kbibby@avemacorp.com>
2019-09-30 16:13:23 -0400

    TempProduce created

---
+++ TestApplication/src/testapplication/temp/TempProduce.java
@@ -0,0 +1,14 @@
+package testapplication.temp;
+
+/**
+ *
+ * @author Kyle
+ * @date Sep 30, 2019
+ */
+public class TempProduce {
+   
+    @Override
+   public String toString() {
+        return "Apple, Orange, Banana, Corn, Pumpkin, Celery";
+    } 
+}
